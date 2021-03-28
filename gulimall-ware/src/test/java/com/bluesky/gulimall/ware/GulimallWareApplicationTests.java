6a842550149d93dde04ab262f1f6f3b045ad47bf
wanggaofei <972617883@qq.com>
2021-03-28 15:09:22 +0800

    fist commit

---
+++ gulimall-ware/src/test/java/com/bluesky/gulimall/ware/GulimallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bluesky.gulimall.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
