29044856c2d0d234b057dd2b4c17ce27ed067116
wanggaofei <972617883@qq.com>
2021-03-28 22:55:44 +0800

    测试远程调用，修改springcloud、springcloud-alibaba、springboot版本对应关系

--- gulimall-member/src/main/java/com/bluesky/gulimall/member/controller/MemberController.java
+++ gulimall-member/src/main/java/com/bluesky/gulimall/member/controller/MemberController.java
@@ -3,6 +3,7 @@ package com.bluesky.gulimall.member.controller;
 import java.util.Arrays;
 import java.util.Map;
 
+import com.bluesky.gulimall.member.feign.CouponFeignService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -30,6 +31,16 @@ public class MemberController {
     @Autowired
     private MemberService memberService;
 
+    @Autowired
+    CouponFeignService couponFeignService;
+
+    @RequestMapping("/coupons")
+    public R test(){
+        MemberEntity memberEntity = new MemberEntity();
+//        memberEntity.setNickname("张三");
+        R m = couponFeignService.membercooupons();
+        return R.ok().put("member",memberEntity).put("coupons",m.get("coupons"));
+    }
     /**
      * 列表
      */
---
+++ gulimall-member/src/main/java/com/bluesky/gulimall/member/feign/CouponFeignService.java
@@ -0,0 +1,12 @@
+package com.bluesky.gulimall.member.feign;
+
+import com.bluesky.common.utils.R;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@FeignClient("gulimall-coupon")
+public interface CouponFeignService {
+
+    @RequestMapping("/coupon/coupon/member/list")
+    R membercooupons();
+}
