e7f0aebfd1c936d450beae1d8f477ff7fd6f1333
wanggaofei <972617883@qq.com>
2021-05-16 16:51:03 +0800

    新增OSS上传测试

--- gulimall-product/src/test/java/com/bluesky/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/bluesky/gulimall/product/GulimallProductApplicationTests.java
@@ -1,16 +1,59 @@
 package com.bluesky.gulimall.product;
 
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClient;
 import com.bluesky.gulimall.product.entity.BrandEntity;
 import com.bluesky.gulimall.product.service.BrandService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
 @SpringBootTest
 class GulimallProductApplicationTests {
 
     @Autowired
     BrandService brandService;
+
+    @Autowired
+    OSSClient ossClient;
+
+    @Test
+    public void voidTestUpload() throws FileNotFoundException {
+        // yourEndpoint填写Bucket所在地域对应的Endpoint。以华东1（杭州）为例，Endpoint填写为https://oss-cn-hangzhou.aliyuncs.com。
+        String endpoint = "oss-cn-beijing.aliyuncs.com";
+        // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
+        String accessKeyId = "LTAI5tKxRGqCga2UhqJuE5wc";
+        String accessKeySecret = "1f48GgnIpKb4wudHfY95fLhqt2vh3k";
+
+        // 创建OSSClient实例。
+        OSS ossClient = new OSSClient(endpoint, accessKeyId, accessKeySecret);
+
+        // 填写本地文件的完整路径。如果未指定本地路径，则默认从示例程序所属项目对应本地路径中上传文件流。
+        InputStream inputStream = new FileInputStream("/Users/wanggaofei/Downloads/aa.pdf");
+        // 填写Bucket名称和Object完整路径。Object完整路径中不能包含Bucket名称。
+        ossClient.putObject("bluesky-product", "aa.pdf", inputStream);
+
+        // 关闭OSSClient。
+        ossClient.shutdown();
+        System.out.println("上传完成");
+    }
+    @Test
+    public void voidTestBootUpload() throws FileNotFoundException {
+
+
+        // 填写本地文件的完整路径。如果未指定本地路径，则默认从示例程序所属项目对应本地路径中上传文件流。
+        InputStream inputStream = new FileInputStream("/Users/wanggaofei/Downloads/aa.pdf");
+        // 填写Bucket名称和Object完整路径。Object完整路径中不能包含Bucket名称。
+        ossClient.putObject("bluesky-product", "aa.pdf", inputStream);
+
+        // 关闭OSSClient。
+        ossClient.shutdown();
+        System.out.println("上传完成");
+    }
 
     @Test
     void contextLoads() {
